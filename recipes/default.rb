f4b0dac01253a943dea28205d3629fa77cc7ce53
Derek <pdc2189@icloud.com>
2018-08-25 11:31:04 -0400

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# This is my first attempt at using nano...not really
+# This is my second attempt...yata yata not really
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+    content '<h1>Hello, world </h1>'
+end
+
+service 'httpd' do
+    action [:enable , :start]
+end
